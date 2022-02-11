be20f5a01426c1a03767fce02a3946cf820f6fce
Aaron <cooper.a.m@hotmail.com>
2022-02-11 19:16:32 +0000

    Restructure (#37)
    New folder structure

--- Source/Backend/AddVitalServiceAsAService.ps1
+++
@@ -1,9 +0,0 @@
-
-task InstallService {
-    New-Service -Name "VitalService" -StartupType "Automatic" -ErrorAction "Stop" -BinaryPathName ./bin/VitalService.exe
-}
-
-// start windows service
-task StartService {
-    Start-Service -Name "VitalService" -ErrorAction "Stop"
-}
