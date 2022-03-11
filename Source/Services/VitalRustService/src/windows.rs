76a4de737babb08d792137fd5aaa5fc6b5ec38b9
Aaron <cooper.a.m@hotmail.com>
2022-03-11 00:35:21 +0000

    get process path

--- Source/Services/VitalRustService/src/windows.rs
+++ Source/Services/VitalRustService/src/windows.rs
@@ -1,13 +1,24 @@
-use std::{collections::HashMap, ops::Deref, sync::Mutex};
+use std::{collections::HashMap, mem::size_of_val, str::from_utf8, sync::Mutex};
 
 use once_cell::sync::OnceCell;
-use windows::{Win32::Foundation::*, Win32::UI::WindowsAndMessaging::*};
+use windows::{
+    Win32::Foundation::*,
+    Win32::{
+        System::Diagnostics::ToolHelp::{
+            CreateToolhelp32Snapshot, Module32First, MODULEENTRY32, TH32CS_SNAPMODULE,
+            TH32CS_SNAPMODULE32,
+        },
+        UI::WindowsAndMessaging::*,
+    },
+};
 
 static WINDOW_TITLES: OnceCell<Mutex<HashMap<u32, String>>> = OnceCell::new();
 
 // function that gets all running windows and their titles
 #[cfg(target_os = "windows")]
-pub fn get_mainwindowtitles() -> HashMap<u32, String> {
+pub fn get_processdata() -> HashMap<u32, WinObject> {
+    let main_window_titles;
+
     unsafe {
         let mut guard = WINDOW_TITLES
             .get_or_init(|| Mutex::new(HashMap::new()))
@@ -27,10 +38,22 @@ pub fn get_mainwindowtitles() -> HashMap<u32, String> {
             .lock()
             .unwrap();
 
-        let toreturn = guard.deref().clone();
-        std::mem::drop(guard);
-        return toreturn;
+        main_window_titles = guard.clone();
+        std::mem::drop(&guard);
     }
+
+    let mut main_window_data = HashMap::new();
+    for ele in main_window_titles {
+        main_window_data.insert(
+            ele.0,
+            WinObject {
+                title: ele.1,
+                path: get_proc_path(ele.0),
+            },
+        );
+    }
+
+    return main_window_data;
 }
 
 fn lpfn() -> windows::Win32::UI::WindowsAndMessaging::WNDENUMPROC {
@@ -54,7 +77,6 @@ unsafe extern "system" fn callback(hwnd: HWND, _: LPARAM) -> BOOL {
     let mut id: u32 = 0;
 
     GetWindowThreadProcessId(hwnd, &mut id);
-    //let exists = guard.keys().find(|&x| *x == id);
     if (text.contains("Default IME")
         || text.contains("MSCTFIME UI")
         || text.contains("GDI+ Window")
@@ -62,74 +84,36 @@ unsafe extern "system" fn callback(hwnd: HWND, _: LPARAM) -> BOOL {
         == false
         && IsWindowVisible(hwnd) == true
     {
-        guard.insert(id, text.clone());
+        guard.insert(id, text);
     }
     std::mem::drop(guard);
 
     return true.into();
 }
 
-// function that takes hwnd and returns process id
-fn get_pid(hwnd: HWND) -> u32 {
+fn get_proc_path(pid: u32) -> Option<String> {
+    let mut path = None;
     unsafe {
-        let mut pid: u32 = 0;
-        GetWindowThreadProcessId(hwnd, &mut pid);
-        return pid;
+        let h_snap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, pid);
+        if h_snap != INVALID_HANDLE_VALUE {
+            let mut mod_entry: MODULEENTRY32 = MODULEENTRY32 {
+                ..Default::default()
+            };
+            mod_entry.dwSize = size_of_val(&mod_entry) as u32;
+            if Module32First(h_snap, &mut mod_entry).as_bool() {
+                let char_vec = mod_entry.szExePath.iter().map(|f| f.0).collect::<Vec<u8>>();
+
+                path = match from_utf8(&char_vec) {
+                    Ok(s) => Some(String::from(s.to_string().trim_end_matches(char::from(0)))),
+                    Err(_) => None,
+                };
+            }
+        }
+        CloseHandle(h_snap);
     }
+    return path;
 }
-
-/*
-
-pub fn f() -> bool {
-    unsafe {
-        let hProcessSnap = windows::Win32::System::Diagnostics::ToolHelp::CreateToolhelp32Snapshot(
-            windows::Win32::System::Diagnostics::ToolHelp::TH32CS_SNAPPROCESS,
-            0,
-        );
-
-        let mut pe32: windows::Win32::System::Diagnostics::ToolHelp::PROCESSENTRY32;
-
-        if hProcessSnap == INVALID_HANDLE_VALUE {
-            error!("CreateToolhelp32Snapshot (of processes)");
-            return false;
-        }
-        pe32.dwSize =
-            size_of::<windows::Win32::System::Diagnostics::ToolHelp::PROCESSENTRY32>() as u32;
-
-        if windows::Win32::System::Diagnostics::ToolHelp::Process32First(hProcessSnap, &mut pe32)
-            == false
-        {
-            error!("Process32First"); // show cause of failure
-            windows::Win32::Foundation::CloseHandle(hProcessSnap); // clean the snapshot object
-            return false;
-        }
-
-        let mut list = Vec::new();
-        loop {
-            let mut dwPriorityClass = 0;
-            let hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, pe32.th32ProcessID);
-            if hProcess.is_invalid() {
-                error!("OpenProcess");
-                continue;
-            }
-
-            list.push(ProcessTitle {
-                pid: pe32.th32ProcessID,
-                title: Some(" ".to_string()),
-            });
-
-            if windows::Win32::System::Diagnostics::ToolHelp::Process32Next(hProcessSnap, &mut pe32)
-                == false
-            {
-                break;
-            }
-        }
-
-        return true;
-    }
-} */
-
-struct ProcessTitle {
-    pub pid: u32,
-    pub title: Option<String>,
+pub struct WinObject {
+    pub title: String,
+    pub path: Option<String>,
 }
