b26a9bc7dcda076aef2a0c3a7b6bdbd4dcee346f
Snazzie <cooper.a.m@hotmail.com>
2022-07-10 16:30:52 +0100

    vite react plugin

--- Source/ClientApp/vite.config.ts
+++ Source/ClientApp/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "vite-preset-react";
 import progress from "vite-plugin-progress";
 import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), progress(), eslint()],
+    plugins: [react({ removeDevtoolsInProd: true, injectReact: true }), progress(), eslint()],
     define: {
         __APP_VERSION__: JSON.stringify(process.env.npm_package_version)
     }
