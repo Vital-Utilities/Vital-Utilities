93894413d2d36bd3bdcee3dbd3853f0cd7b1c315
Snazzie <cooper.a.m@hotmail.com>
2022-07-11 16:06:37 +0100

    fast refresh

--- Source/ClientApp/vite.config.ts
+++ Source/ClientApp/vite.config.ts
@@ -5,7 +5,7 @@ import eslint from "vite-plugin-eslint";
 import vis from "rollup-plugin-visualizer";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react({ removeDevtoolsInProd: true, injectReact: true }), progress(), eslint({ cache: true, fix: true }), vis()],
+    plugins: [react({ removeDevtoolsInProd: true, injectReact: true, reactPluginOptions: { fastRefresh: true } }), progress(), eslint({ cache: true, fix: true }), vis()],
     define: {
         __APP_VERSION__: JSON.stringify(process.env.npm_package_version)
     }
