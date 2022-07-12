d1c6a13ecbb116ad4ec6a107a26f3bb87eb9ce3b
Snazzie <cooper.a.m@hotmail.com>
2022-07-13 00:56:59 +0100

    refactor

--- Source/Services/VitalRustService/src/nvidia.rs
+++ Source/Services/VitalRustService/src/nvidia.rs
@@ -1,96 +1,151 @@
 use std::collections::HashMap;
 
 use log::error;
-use nvml::{struct_wrappers::device::ProcessUtilizationSample, Nvml};
-use vital_service_api::models::{GpuUsage, LoadData, PcieThroughPut};
+use nvml::{struct_wrappers::device::ProcessUtilizationSample, Device, Nvml};
+use vital_service_api::models::{GpuClockSpeeds, GpuUsage, LoadData, PcieThroughPut};
 
-pub fn get_gpu_util(device: &nvml::Device) -> GpuUsage {
-    let utilization = device.utilization_rates().unwrap();
-    let mem = device.memory_info().unwrap();
-    let mut fans = HashMap::<String, f32>::new();
+pub fn get_gpu_util(nvml: &Option<Nvml>) -> Vec<GpuUsage> {
+    if nvml.is_none() {
+        return Vec::new();
+    }
+    let mut list = Vec::new();
 
-    for i in 0.. {
-        match device.fan_speed(i) {
-            Ok(speed) => {
-                fans.insert(format!("Fan {}", i), speed as f32);
+    if let Some(nvml) = nvml {
+        let count = nvml.device_count();
+        if count.is_err() {
+            error!("{:?}", count.err().unwrap());
+            return Vec::new();
+        }
+
+        for device_index in 0..count.unwrap() {
+            let device = nvml.device_by_index(device_index);
+            if device.is_err() {
+                error!("{:?}", device.unwrap_err());
+                continue;
             }
-            Err(_) => {
-                break;
+            let device = device.unwrap();
+            let fan_count = device.num_fans();
+            let mut fan_percentages = HashMap::new();
+            if fan_count.is_ok() {
+                for fan_index in 0..fan_count.unwrap() {
+                    match device.fan_speed(fan_index) {
+                        Ok(speed) => {
+                            fan_percentages.insert(format!("Fan {}", fan_index), speed as f32);
+                        }
+                        Err(_) => {
+                            error!(
+                                "Failed to get fan speed for device {:?}. Fan index {:?}",
+                                device.name(),
+                                fan_index
+                            );
+                        }
+                    }
+                }
             }
+            let mut mem_total: Option<i64> = None;
+            let mut mem_used: Option<i64> = None;
+
+            match device.memory_info() {
+                Ok(info) => {
+                    mem_total = Some(info.total as i64);
+                    mem_used = Some(info.used as i64);
+                }
+                Err(e) => {
+                    error!(
+                        "Failed to get memory info for device {:?}. Error: {:?}",
+                        device.name(),
+                        e
+                    );
+                }
+            }
+
+            let mut temperature_readings = HashMap::new();
+
+            match device.temperature(nvml::enum_wrappers::device::TemperatureSensor::Gpu) {
+                Ok(temp) => {
+                    temperature_readings.insert("GPU".to_string(), temp as f32);
+                }
+                Err(e) => {
+                    error!(
+                        "Failed to get temperature for device {:?}. Error: {:?}",
+                        device.name(),
+                        e
+                    );
+                }
+            }
+
+            let data = GpuUsage {
+                name: match device.name() {
+                    Ok(name) => Some(name),
+                    Err(_) => None,
+                },
+                temperature_readings: temperature_readings,
+                total_memory_bytes: mem_total,
+                memory_used_bytes: mem_used,
+
+                fan_percentage: Some(fan_percentages),
+                power_draw_watt: match device.power_usage() {
+                    Ok(milliwatt) => Some((milliwatt / 1000) as i32),
+                    Err(_) => None,
+                },
+                clock_speeds: Some(Box::new(get_gpu_clock_speeds(&device))),
+                load: Some(Box::new(get_gpu_load(&device))),
+                pc_ie: Some(Box::new(get_pcie_throughput(&device))),
+            };
+            list.push(data);
         }
     }
+    return list;
+}
 
-    return GpuUsage {
-        name: match device.name() {
-            Ok(name) => Some(name),
-            Err(e) => {
-                error!("Could not get GPU name: {}", e);
-                None
-            }
-        },
+fn get_gpu_load(device: &Device) -> LoadData {
+    return LoadData {
+        core_percentage: Some(device.utilization_rates().unwrap().gpu as f32),
+        frame_buffer_percentage: None,
+        video_engine_percentage: None,
+        bus_interface_percentage: None,
+        memory_used_percentage: None,
+        memory_controller_percentage: Some(device.utilization_rates().unwrap().gpu as f32),
+        cuda_percentage: None,
+        three_d_percentage: None,
+    };
+}
 
-        load: Some(Box::new(LoadData {
-            core_percentage: Some(utilization.gpu as f32),
-            frame_buffer_percentage: None,
-            video_engine_percentage: None,
-            bus_interface_percentage: None,
-            memory_used_percentage: Some((mem.used as f32 / mem.total as f32) * 100.0),
-            memory_controller_percentage: Some(utilization.memory as f32),
-            cuda_percentage: None,
-            three_d_percentage: None,
-        })),
-        core_clock_mhz: match device.clock(
-            nvml::enum_wrappers::device::Clock::Graphics,
-            nvml::enum_wrappers::device::ClockId::Current,
-        ) {
-            Ok(clock) => Some(clock as i32),
+fn get_gpu_clock_speeds(device: &Device) -> GpuClockSpeeds {
+    return GpuClockSpeeds {
+        memory_clock_mhz: match device.clock_info(nvml::enum_wrappers::device::Clock::Memory) {
+            Ok(speed) => Some(speed as i32),
             Err(_) => None,
         },
-        memory_clock_mhz: match device.clock(
-            nvml::enum_wrappers::device::Clock::Memory,
-            nvml::enum_wrappers::device::ClockId::Current,
-        ) {
-            Ok(clock) => Some(clock as i32),
+        compute_clock_mhz: match device.clock_info(nvml::enum_wrappers::device::Clock::SM) {
+            Ok(speed) => Some(speed as i32),
             Err(_) => None,
         },
-        shader_clock_mhz: match device.clock(
-            nvml::enum_wrappers::device::Clock::SM,
-            nvml::enum_wrappers::device::ClockId::Current,
-        ) {
-            Ok(clock) => Some(clock as i32),
+        graphics_clock_mhz: match device.clock_info(nvml::enum_wrappers::device::Clock::Graphics) {
+            Ok(speed) => Some(speed as i32),
             Err(_) => None,
         },
-        temperature_readings: match device
-            .temperature(nvml::enum_wrappers::device::TemperatureSensor::Gpu)
+        video_clock_mhz: match device.clock_info(nvml::enum_wrappers::device::Clock::Video) {
+            Ok(speed) => Some(speed as i32),
+            Err(_) => None,
+        },
+    };
+}
+
+fn get_pcie_throughput(device: &Device) -> PcieThroughPut {
+    return PcieThroughPut {
+        pc_ie_rx_bytes_per_second: match device
+            .pcie_throughput(nvml::enum_wrappers::device::PcieUtilCounter::Receive)
         {
-            Ok(temp) => {
-                let mut temperature_readings = HashMap::new();
-                temperature_readings.insert("GPU".to_string(), temp as f32);
-                temperature_readings
-            }
-            Err(_) => HashMap::new(),
-        },
-        power_draw_watt: match device.power_usage() {
-            Ok(power) => Some(power as i32),
+            Ok(bytes) => Some(bytes as i64),
             Err(_) => None,
         },
-        pc_ie: Some(Box::new(PcieThroughPut {
-            pc_ie_rx_bytes_per_second: match device
-                .pcie_throughput(nvml::enum_wrappers::device::PcieUtilCounter::Receive)
-            {
-                Ok(throughput) => Some((throughput * 1000).into()),
-                Err(_) => None,
-            },
-            pc_ie_tx_bytes_per_second: match device
-                .pcie_throughput(nvml::enum_wrappers::device::PcieUtilCounter::Send)
-            {
-                Ok(throughput) => Some((throughput * 1000).into()),
-                Err(_) => None,
-            },
-        })),
-        fan_percentage: Some(fans),
-        memory_used_bytes: Some(mem.used as i64),
-        total_memory_bytes: Some(mem.total as i64),
+        pc_ie_tx_bytes_per_second: match device
+            .pcie_throughput(nvml::enum_wrappers::device::PcieUtilCounter::Send)
+        {
+            Ok(bytes) => Some(bytes as i64),
+            Err(_) => None,
+        },
     };
 }
 
