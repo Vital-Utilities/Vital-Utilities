9a84a23e72c39529b5ec04735ff8a1eb125f23cc
Snazzie <cooper.a.m@hotmail.com>
2022-07-24 00:31:10 +0100

    update Vite

--- Source/ClientApp/vite.config.ts
+++ Source/ClientApp/vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
-import react from "vite-preset-react";
+import react from "@vitejs/plugin-react";
 import progress from "vite-plugin-progress";
 import eslint from "vite-plugin-eslint";
 import vis from "rollup-plugin-visualizer";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react({ removeDevtoolsInProd: true, injectReact: true, reactPluginOptions: { fastRefresh: true } }), progress(), eslint({ cache: true, fix: true }), vis()],
+    plugins: [react({ fastRefresh: true }), progress(), eslint({ cache: true, fix: true }), vis()],
     define: {
         __APP_VERSION__: JSON.stringify(process.env.npm_package_version)
+    },
+    server: {
+        port: 3000
     }
 });
